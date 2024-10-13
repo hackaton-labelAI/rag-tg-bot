d6caee537f6981ab7c235d497ece6e6678f5cf67
anasenicenkova <senichenkova02@inbox.ru>
2024-10-13 07:02:09 +0300

    current state

--- app/api_interaction.py
+++ app/api_interaction.py
@@ -1,16 +1,33 @@
-import requests
 import json
 
+import requests
+
+from app.config import SERVER
 
 server_address = ''
+
 
 def proccess_question(question: str):
     headers = {
         'accept': 'application/json',
     }
 
-    data = ''# pass
-    response = requests.post(url=f"{server_address}/api/chat", data=data, headers=headers)
+    data = {
+        "session_id": None,
+        "context": [{
+            "text_data": {
+                "role": "user",
+                "content": question
+            },
+            "rag_data": None
+        }]
+    }
+    chunks = requests.post(url=f'{SERVER}/api/chat', data=data, headers=headers)['data']
+    data = {
+        'question': question,
+        'chunks': chunks
+    }
+    response = requests.post(url=f'{SERVER}/api/telegram_get_response', data=data)
 
     return json.loads(response.text)
 
--- app/config.py
+++ app/config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = '7616841236:AAFogzwqP1qy-0DWK4dnsizH2g4okDS_bAc'
 WHAT_DO_YOU_WANT_STR = 'Что ты хочешь сделать?'
+SERVER = 'http://0.0.0.0:8080'
